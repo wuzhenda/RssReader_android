486a718516b1edbf8b92222b6a4d022a81f2b7b8
Henry.Wu <Henry.Wu@benq.com>
2013-04-03 15:29:48 +0800

    initial version,not build successfull

---
+++ bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/home/Henry.Wu/Yunio/work_git/android-rss-read-only/bin/AndroidManifest.xml : \
+/home/Henry.Wu/Yunio/work_git/android-rss-read-only/AndroidManifest.xml \
---
+++ gen/org/devtcg/rssreader/service/IReaderService.java.d
@@ -0,0 +1,3 @@
+/home/Henry.Wu/Yunio/work_git/android-rss-read-only/gen/org/devtcg/rssreader/service/IReaderService.java: \
+  /home/Henry.Wu/Yunio/work_git/android-rss-read-only/src/org/devtcg/rssreader/service/IReaderService.aidl 
+
