30708e4de5f09b7724cbfda1687345f29a747673
Henry.Wu <Henry.Wu@benq.com>
2013-04-15 13:14:32 +0800

    rss reader from aosp

--- bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/Henry.Wu/Yunio/work_git/android-rss-read-only/bin/AndroidManifest.xml : \
-/home/Henry.Wu/Yunio/work_git/android-rss-read-only/AndroidManifest.xml \
--- gen/org/devtcg/rssreader/service/IReaderService.java.d
+++
@@ -1,3 +0,0 @@
-/home/Henry.Wu/Yunio/work_git/android-rss-read-only/gen/org/devtcg/rssreader/service/IReaderService.java: \
-  /home/Henry.Wu/Yunio/work_git/android-rss-read-only/src/org/devtcg/rssreader/service/IReaderService.aidl 
-
